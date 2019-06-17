88b2139b4e1c17ea3cc3e3972f70bdbca32cb0be
icecream <zanjarwhite@gmail.com>
2019-06-17 16:21:02 +0600

    add

--- fametame/urls.py
+++ fametame/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
@@ -10,3 +11,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index),
 ]
+
+urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
